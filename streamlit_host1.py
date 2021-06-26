a796d55a07f2ccfebd5e2742d2b94ebd7bdfc111
Vikas Thapa <36027760+vikas08thapa@users.noreply.github.com>
2021-06-26 13:04:37 +0530

    Add files via upload

---
+++ streamlit_host1.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+
+model = tf.keras.models.load_model("saved_model/TLmodel1.h5")
+### load file
+uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+
+map_dict = {0: 'buildings',
+            1: 'forest',
+            2: 'glacier',
+            3: 'mountain',
+            4: 'sea',
+            5: 'street'}
+
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    resized = cv2.resize(opencv_image,(150,150))
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="RGB")
+
+    resized = mobilenet_v2_preprocess_input(resized)
+    img_reshape = resized[np.newaxis,...]
+
+    Genrate_pred = st.button("Generate Prediction")    
+    if Genrate_pred:
+        prediction = model.predict(img_reshape).argmax()
+        st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
